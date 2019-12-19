3159b24eb75b92fa905114288a5ede4530a51aa2
Nick <sargentnargent@gmail.com>
2019-12-19 04:48:16 -0500

    refined registration, added Joi

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,9 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+const AuthenticationConteollerPolicy = require('./policies/AuthenticationControllerPolicy')
 
 module.exports = (app) => {
   app.post('/register',
+    AuthenticationConteollerPolicy.register,
     AuthenticationController.register)
 }
