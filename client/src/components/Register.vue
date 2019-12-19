95d0c51b33d7dbf28af6af21b6b5d5bfca6993c3
Nick <sargentnargent@gmail.com>
2019-12-19 09:22:10 -0500

    strugled with styling for too long

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,32 +1,36 @@
 <template>
   <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
+    <v-flex offset-xs3>
+      <v-flex xs8>
+      <v-card :elevation="2" height="300" width = "600">
+        <v-toolbar flat dense dark>
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
-        <div class="pl-4 pr-4 pt-2 pb-2">
-            <input
-          type="email"
-          name="email"
-          v-model="email"
-          placeholder="email" />
-         <br>
-         <input
-          type="password"
-          name="password"
-          v-model="password"
-          placeholder="password" />
-         <br>
-         <div class="error" v-html="error" />
-         <br>
-         <v-btn
-          class="cyan"
-          @click="register">
-          Register
-         </v-btn>
-        </div>
-          </div>
+          <form
+            name="fit-tracker-form"
+            autocomplete="off">
+            <v-text-field
+              label="Email"
+              v-model="email"
+              class="styled-input"
+            ></v-text-field>
+            <br>
+            <v-text-field
+              label="Password"
+              v-model="password"
+              class="styled-input"
+            ></v-text-field>
+          </form>
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            dark
+            @click="register">
+            Register
+          </v-btn>
+      </v-card>
+      </v-flex>
     </v-flex>
   </v-layout>
 </template>
@@ -58,6 +62,10 @@ export default {
 
 <style scoped>
 .error {
-  color: red;
+  color: darkred;
+}
+.styled-input{
+  padding-left: 20px;
+  padding-right: 20px;
 }
 </style>
