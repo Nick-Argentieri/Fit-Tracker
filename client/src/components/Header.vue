12b52287b726961c9d47d93adc8b83d8759c810d
Nick <sargentnargent@gmail.com>
2019-12-20 03:53:03 -0500

    links between registration and home on tool bar

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,20 +1,36 @@
 <template>
    <v-app>
      <v-card>
-      <v-toolbar fixed dark>
+      <v-toolbar fixed dark dense>
         <v-toolbar-title class="mr-4">
-          <span>
+          <span
+          class="home"
+          @click="navigateTo({name: 'root'})">
             Fitness Tracker
           </span>
         </v-toolbar-title>
+        <v-toolbar-items>
+          <v-btn flat>
+            Browse
+          </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn flat
+          @click="navigateTo({name: 'register'})">
+              Sign Up
+          </v-btn>
+        </v-toolbar-items>
       </v-toolbar>
-      <v-spacer></v-spacer>
      </v-card>
   </v-app>
 </template>
 <script>
 export default {
-  data () {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
   }
 }
 </script>
@@ -22,5 +38,8 @@ export default {
 .toolbar__content{
   color: #FFFFFF;
   margin-left: 10%;
+}
+.home {
+  cursor: pointer;
 }
 </style>
